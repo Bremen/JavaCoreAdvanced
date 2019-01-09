04c95c19bee036077bfcd2dd13fa20d125b8eeb8
Boris Remen <boris.remen@gmail.com>
2019-01-09 07:29:14 +0300

    task 1 was done (About counting repeats in word array)

---
+++ Lesson3.Collections/src/RepeatCounter.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RepeatCounter {
+    public static void main(String[] args) {
+        String words = "морковь, репа, редька, редис, петрушка,пастернак,сельдерей, хрен,брюква, редька,морковь, свёкла, репа, брюква, редька, редис, петрушка, пастернак, сельдерей, хрен, сельдерей, хрен, редис, петрушка, пастернак, морковь, сельдерей, хрен, сельдерей, хрен";
+        String[] wordArray = words.split(",\\s?");
+
+        repeatsReport(findRepeats(wordArray));
+    }
+
+    private static void repeatsReport(Map<String, Integer> repeats) {
+        for (String word : repeats.keySet()) {
+            System.out.println(word + "  " + repeats.get(word));
+        }
+    }
+
+    private static Map<String, Integer> findRepeats(String[] wordArray) {
+        Map<String, Integer> unicValueRepeats = new HashMap<>();
+
+        for (String word : wordArray) {
+            if (unicValueRepeats.containsKey(word)){
+                unicValueRepeats.computeIfPresent(word, (k, v) -> v + 1);
+            }else{
+                unicValueRepeats.put(word, 1);
+            }
+        }
+
+        return unicValueRepeats;
+    }
+}
